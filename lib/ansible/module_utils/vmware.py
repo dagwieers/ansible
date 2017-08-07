5c685e1f521926f3c6908710aba8f8e24c9feba2
Dag Wieers <dag@wieers.com>
2017-08-07 22:57:22 +0200

    A few more changes I would make
    @Akasurde This is what I meant.

--- lib/ansible/module_utils/vmware.py
+++ lib/ansible/module_utils/vmware.py
@@ -31,8 +31,8 @@ try:
 except ImportError:
     HAS_PYVMOMI = False
 
-from ansible.module_utils.urls import fetch_url
 from ansible.module_utils.six import integer_types, iteritems, string_types
+from ansible.module_utils.urls import fetch_url
 
 
 class TaskError(Exception):
@@ -384,7 +384,6 @@ def vmware_argument_spec():
 
 
 def connect_to_api(module, disconnect_atexit=True):
-
     hostname = module.params['hostname']
     username = module.params['username']
     password = module.params['password']
@@ -394,24 +393,20 @@ def connect_to_api(module, disconnect_atexit=True):
         module.fail_json(msg='pyVim does not support changing verification mode with python < 2.7.9. Either update '
                              'python or or use validate_certs=false')
 
-    service_instance = None
     ssl_context = None
-    error_msg = "Unable to connect to vCenter or ESXi API on TCP/443"
-
     if not validate_certs:
         ssl_context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
         ssl_context.verify_mode = ssl.CERT_NONE
 
+    service_instance = None
     try:
         service_instance = connect.SmartConnect(host=hostname, user=username, pwd=password, sslContext=ssl_context)
     except vim.fault.InvalidLogin as e:
-        module.fail_json(msg="%s : %s" % (error_msg, e.msg))
-
+        module.fail_json(msg="Unable to log on to vCenter or ESXi API at %s as %s: %s" % (hostname, username, e.msg))
     except (requests.ConnectionError, ssl.SSLError) as e:
-        module.fail_json(msg="%s : %s" % (error_msg, e))
+        module.fail_json(msg="Unable to connect to vCenter or ESXi API at %s on TCP/443: %s" % (hostname, e))
     except Exception as e:
-        error_msg += " due to %s " % e
-        module.fail_json(msg=error_msg)
+        module.fail_json(msg="Unknown error connecting to vCenter or ESXi API at %s: %s" % (hostname, e))
 
     if service_instance is None:
         module.fail_json(msg=error_msg)
