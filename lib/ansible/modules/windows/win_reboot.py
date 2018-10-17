87a2849cd82993ba0d747dfb41dd98721200ffe3
Dag Wieers <dag@wieers.com>
2018-10-17 20:26:17 +0200

    win_reboot: Add more specific mitigation notes
    As discussed in the Windows WG meeting, we cannot implement a better fix
    as we cannot detect that updates have been applied. So until we can
    improve this we have to make users aware of this common issue.

--- lib/ansible/modules/windows/win_reboot.py
+++ lib/ansible/modules/windows/win_reboot.py
@@ -32,14 +32,14 @@ options:
   shutdown_timeout:
     description:
     - Maximum seconds to wait for shutdown to occur.
-    - Increase this timeout for very slow hardware, large update applications, etc.
-    - This option has been removed since Ansible 2.5 as the win_reboot behavior has changed.
+    - This option has been removed since Ansible 2.5 as the C(win_reboot) behavior has changed.
     type: int
     default: 600
     aliases: [ shutdown_timeout_sec ]
   reboot_timeout:
     description:
     - Maximum seconds to wait for machine to re-appear on the network and respond to a test command.
+    - Increase this timeout for very slow hardware, large updates, or when WinRM has a delayed startup.
     - This timeout is evaluated separately for both network appearance and test command success (so maximum clock time is actually twice this value).
     type: int
     default: 600
@@ -62,6 +62,9 @@ options:
     default: Reboot initiated by Ansible
 notes:
 - If a shutdown was already scheduled on the system, C(win_reboot) will abort the scheduled shutdown and enforce its own shutdown.
+- If updates were applied to Windows, additional pre-boot and post-tasks may increase the boot time drastically.
+  The default I(reboot_timeout) is 600 seconds, which may not cover this additional processing and can result in unexpected behavior.
+  Check the examples for ways to mitigate this. More information is available from U(https://github.com/ansible/ansible/issues/47229).
 - Beware that when C(win_reboot) returns, the Windows system may not have settled yet and some base services could be in limbo.
   This can result in unexpected behavior. Check the examples for ways to mitigate this.
 - For non-Windows targets, use the M(reboot) module instead.
@@ -73,10 +76,6 @@ EXAMPLES = r'''
 - name: Reboot the machine with all defaults
   win_reboot:
 
-- name: Reboot a slow machine that might have lots of updates to apply
-  win_reboot:
-    reboot_timeout: 3600
-
 # Install a Windows feature and reboot if necessary
 - name: Install IIS Web-Server
   win_feature:
@@ -87,18 +86,29 @@ EXAMPLES = r'''
   win_reboot:
   when: iis_install.reboot_required
 
-# One way to ensure the system is reliable, is to add a delay before running the next task
+
+## Windows may have updates applied, causing additional pre- and post processing
+
+# Mitigation: Increase the reboot_timeout to ensure we survive the extra boot-time
+- name: Reboot a machine that may have updates applied and needs more time
+  win_reboot:
+    reboot_timeout: 3600
+
+
+## Windows may need time to settle while WinRM is already available
+
+# Mitigation 1: Add a post-reboot delay before running the next task
 - name: Reboot a machine that takes time to settle after being booted
   win_reboot:
     post_reboot_delay: 120
 
-# Alternatively, you can set WinRM to a delayed startup
+# Mitigation 2: Delay the WinRM restart until after the base services
 - name: Ensure WinRM starts when the system has settled and is ready to work reliably
   win_service:
     name: WinRM
     start_mode: delayed
 
-# Or you can make win_reboot validate exactly what you need to work before running the next task
+# Mitigation 3: Let win_reboot validate whatever is needed for the next task
 - name: Validate that the netlogon service has started, before running the next task
   win_reboot:
     test_command: 'exit (Get-Service -Name Netlogon).Status -ne "Running"'
